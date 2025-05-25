c50f8f7d13ab525b1bd1e8c83e241b79fb1f74d1
Manuel Nunes <marnununes@gmail.com>
2025-05-25 21:17:30 +0200

    Add ability to zoom cameras

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,29 @@ import { Player } from './game/classes/player';
 
 const App: React.FC = () => {
 
-  const [playerMovementSpeedMultiplier, SetPlayerMovementSpeedMultiplier] = useState(Player.MovementSpeedMultiplier)
+  const [playerMovementSpeedMultiplier, SetPlayerMovementSpeedMultiplier] = useState(Player.MovementSpeedMultiplier);
+  const [cameraZoom, setCameraZoom] = useState(1);
+  const [game, setGame] = useState<Phaser.Game | undefined>();
 
   useEffect(() => {
 
-    const game = createPhaserGame('phaser-container');
+    const gameTemp = createPhaserGame('phaser-container');
+    setGame(gameTemp);
 
     return () => {
 
-      game.destroy(true);
+      gameTemp.destroy(true);
 
     };
     
   }, []);
 
-  useEffect(()=>{Player.MovementSpeedMultiplier = playerMovementSpeedMultiplier},[playerMovementSpeedMultiplier])
+  useEffect(()=>{Player.MovementSpeedMultiplier = playerMovementSpeedMultiplier;},[playerMovementSpeedMultiplier]);
+
+  useEffect(()=>{
+    game?.scene.getScene('Game')?.cameras?.cameras?.[0]?.setZoom(cameraZoom);
+
+  },[cameraZoom, game]);
 
   return (
     <div
@@ -35,8 +43,19 @@ const App: React.FC = () => {
         <div
           className='PlayerSettings'>
           <label htmlFor="">Player movement speed multiplier: {playerMovementSpeedMultiplier}</label>
-          <button onMouseDownCapture={()=> SetPlayerMovementSpeedMultiplier(prev => Math.round((prev + 0.1)*10)/10 )}>+</button>
-          <button onMouseDown={()=> SetPlayerMovementSpeedMultiplier(prev => Math.round((prev - 0.1)*10)/10)}>-</button>
+          <div>
+            <button onMouseDownCapture={()=> SetPlayerMovementSpeedMultiplier(prev => Math.round((prev + 0.1)*10)/10 )}>+</button>
+            <button onMouseDown={()=> SetPlayerMovementSpeedMultiplier(prev => Math.round((prev - 0.1)*10)/10)}>-</button>
+          </div>
+        </div>
+
+        <div
+          className='PlayerSettings'>
+          <label htmlFor="">Camera Zoom: {cameraZoom}</label>
+          <div>
+            <button onMouseDownCapture={()=> setCameraZoom(prev => Math.round((prev + 0.1)*10)/10 )}>+</button>
+            <button onMouseDown={()=> setCameraZoom(prev => Math.round((prev - 0.1)*10)/10)}>-</button>
+          </div>
         </div>
 
       </div>
@@ -45,7 +64,7 @@ const App: React.FC = () => {
       
     </div>
 
-  )
-}
+  );
+};
 
 export default App;
